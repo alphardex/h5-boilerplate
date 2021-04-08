bcb8310e0483b2818882a7c7d0eeddb84e8fe914
alphardex <2582347430@qq.com>
2021-04-08 08:50:22 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-// 自定义弹窗
 const myAlert = (msg, cb) => {
   $("#my-alert .my-alert-body").html(msg);
   $("#my-alert").modal();
 };
 
-// 修复ios输入框弹出问题
 const iosInputScrollFix = () => {
   document.querySelectorAll("input, select, textarea").forEach((input) => {
     input.addEventListener("blur", () => {
@@ -13,9 +11,12 @@ const iosInputScrollFix = () => {
   });
 };
 
-// 获取url的qs
 const getURLParameters = (url) =>
-  (url.match(/([^?=&]+)(=([^&]*))/g) || []).reduce((a, v) => ((a[v.slice(0, v.indexOf("="))] = v.slice(v.indexOf("=") + 1)), a), {});
+  (url.match(/([^?=&]+)(=([^&]*))/g) || []).reduce(
+    (a, v) => (
+      (a[v.slice(0, v.indexOf("="))] = v.slice(v.indexOf("=") + 1)), a
+    ),
+    {}
+  );
 
-// 延迟
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
